7d0675456dbeb7d2f86bc938e8105e0fb8d627d2
brendenbokino <bbokino@loyola.edu>
2025-12-03 23:42:14 -0500

    Added update, delete, and read for ReservationDAO

--- src/main/java/edu/loyola/cs485/gfbb/model/dao/ReservationDAO.java
+++ src/main/java/edu/loyola/cs485/gfbb/model/dao/ReservationDAO.java
@@ -9,9 +9,9 @@ public class ReservationDAO extends AbstractDAO<Reservation> {
     @Override
     public void create(Reservation entity) throws SQLException {
         Connection con = getConnection(); // Always open a connection
-
-        String sql = "INSERT INTO Reservation (dt_reservation, status_reservation, num_guests) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO reservation (dt_reservation, status_reservation, num_guests) VALUES (?, ?, ?)";
         PreparedStatement pst = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
         pst.setTimestamp(1, entity.getDateTime());
         pst.setString(2, entity.getStatus());
         pst.setString(3, entity.getStatus());
@@ -27,17 +27,52 @@ public class ReservationDAO extends AbstractDAO<Reservation> {
 
     @Override
     public Reservation read(int ID) throws SQLException {
-        return null;
+        Reservation reservation = new Reservation();
+        Connection con = getConnection();
+        String sql = "SELECT * FROM reservation WHERE id_reservation = ?";
+        PreparedStatement pst = con.prepareStatement(sql);
+
+        pst.setInt(1, ID);
+        ResultSet Rs = pst.executeQuery();
+        if (Rs.next()) { //IF because there should be 1 or 0 results
+            reservation.setId( Rs.getInt("id_reservation") );
+            reservation.setStatus( Rs.getString("status_reservation") );
+            reservation.setDateTime( Rs.getTimestamp("dt_reservation") );
+            reservation.setNumGuests( Rs.getInt("num_guests") );
+        }
+
+        con.close();
+        return reservation;
     }
 
     @Override
     public void update(Reservation entity) throws SQLException {
+        Connection con = getConnection();
 
+        String sql = "UPDATE reservation " +
+                "SET status_reservation = ?, num_guests = ?, dt_reservation = ?" +
+                " WHERE id_reservation = ?";
+        PreparedStatement pst = con.prepareStatement(sql);
+
+        pst.setString(1, entity.getStatus());
+        pst.setInt(2, entity.getNumGuests());
+        pst.setTimestamp(3, entity.getDateTime());
+        pst.setInt(4, entity.getId());
+        pst.executeUpdate();
+
+        con.close();
     }
 
     @Override
-    public void delete(int ID) throws SQLException {
+    public void delete(int id) throws SQLException {
+        Connection con = getConnection();
+        String sql = "DELETE FROM reservation WHERE id_client = ?";
+        PreparedStatement pst = con.prepareStatement(sql);
 
+        pst.setInt(1, id);
+        pst.executeUpdate();
+
+        con.close();
     }
 
     @Override
